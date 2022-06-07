4bd06bab8fc63168653523a843de8dd82b26a3d4
Irene Njeru <irenenjeru8@gmail.com>
2022-06-07 12:19:55 +0300

    commit tree c03c2aa6de690aff30502d572febd8687b4d6009

--- methods.rb
+++ methods.rb
@@ -1,1 +1,16 @@
 # Your code here!
+def greet_programmer
+    puts "Hello, programmer!"
+end
+
+def greet_with_default(name = "Naureen")
+    puts "Hello, #{name}!"
+  end
+
+  def greet_with_default(name = "programmer!")
+    puts "Hello, #{name}!"
+  end 
+
+  def add num1, num2
+    num1 + num2
+  end
